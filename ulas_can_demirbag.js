edc71a375f62ea0242986bfa317a12514f8be0fe
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2025-09-28 13:39:16 +0300

    fetch products data

--- ulas_can_demirbag.js
+++ ulas_can_demirbag.js
@@ -1,9 +1,27 @@
 //Ulaş Can Demirbağ
 ;(() => {
-  const init = () => {
-    self.buildHTML()
-    self.buildCSS()
-    self.setEvents()
+  const ProdcutsUrl =
+    "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json"
+
+  const init = async () => {
+    const products = await fetchProducts()
+    console.log("Products:", products)
+
+    // self.buildHTML()
+    // self.buildCSS()
+    // self.setEvents()
+  }
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(ProdcutsUrl)
+      if (!response.ok) throw new Error(`HTTP ERROR: ${response.status}`)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.log("Ürünler alınamadı:", error)
+      return {}
+    }
   }
 
   const buildHTML = () => {
@@ -12,6 +30,7 @@
                 <h1></h1>
             </div>
         `
+    $(`.product-detail`).append(html)
   }
 
   const buildCSS = () => {
